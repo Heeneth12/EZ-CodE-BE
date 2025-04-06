2b8e3198457a4aca7c92e7ea2c5f0bb3ef11697e
Heeneth12 <heeneth123@gmail.com>
2025-04-06 11:52:38 +0530

    first commit

---
+++ src/test/java/com/ezcode/EzcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ezcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzcodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
